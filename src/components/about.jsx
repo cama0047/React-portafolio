2c2b32838d9a86daec94dc5666fdcbf02b19c152
Camilo CV <cama0047@algonquinlive.com>
2021-06-29 23:32:52 -0400

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
+        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
+        {
+          id: "JavaScript_skill",
+          content: "JavaScript",
+          porcentage: "90%",
+          value: "90"
+        },
+
+        {
+          id: "ReactJS_skill",
+          content: "ReactJS",
+          porcentage: "80%",
+          value: "80"
+        },
+        {
+          id: "Python_skill",
+          content: "Python",
+          porcentage: "75%",
+          value: "75"
+        },
+        { id: "NODE_skill", content: "Node.js", porcentage: "70%", value: "70" },
+        { id: "Databases_skill", content: "Databases", porcentage: "70%", value: "70" },
+        {
+          id: "AWS_skill",
+          content: "AWS",
+          porcentage: "61%",
+          value: "61"
+        },
+        {
+          id: "Wordpress_skill",
+          content: "Wordpress",
+          porcentage: "80%",
+          value: "80"
+        }
+      ],
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            "I'am an engineer with over 5 years of experience in the industry and high-level skill in numerous technologies, including Front and Back-end development and DevOps, seeking to establish a career as a Developer. Web Application Architecture is something with which I am quite familiar and have a lot of expertise. In addition, I have led multiple development teams to outstanding achievements."
+        },
+        {
+          id: "second-p-about",
+          content:
+            "I have had the pleasure of belonging to large companies in different economic sectors, which have allowed me to expand my knowledge and experience in many fields. Likewise in each of them I have been able to learn and experience technologies that allow me to have a wide range of action. "
+        },
+        {
+          id: "third-p-about",
+          content:
+            " "
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="row">
+                      <div
+                        className="col-sm-6 col-md-5"
+                        style={{ margin: "0 auto" }}
+                      >
+                        <div
+                          className="about-img"
+                          style={{ textAlign: "center" }}
+                        >
+                          <img
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="skill-mf">
+                      {/* <p className="title-s">Skill</p> */}
+                      {this.state.skills.map(skill => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <span>{skill.content}</span>{" "}
+                            <span className="pull-right">
+                              {skill.porcentage}
+                            </span>
+                            <div className="progress">
+                              <div
+                                className="progress-bar"
+                                role="progressbar"
+                                style={{ width: skill.porcentage }}
+                                aria-valuenow={skill.value}
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">About Me</h5>
+                      </div>
+                      {this.state.about_me.map(content => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
